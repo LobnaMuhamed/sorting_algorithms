ea5c429039a9219390df5072b574a2f23ac5aa77
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-07-25 06:21:15 -0700

    solve task

---
+++ 0-bubble_sort.c
@@ -0,0 +1,47 @@
+#include "sort.h"
+
+/**
+ * swap - swap two integers
+ * @x: first input integer's pointer
+ * @y: second input integer's pointer
+*/
+
+void swap(int *x, int *y)
+{
+	int temp;
+
+	temp = *x;
+	*x = *y;
+	*y = temp;
+}
+
+/**
+ * bubble_sort - a function that sorts an array of ints in
+ *      ascending order
+ * @array: array input
+ * @size: size of an array
+*/
+
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int swapped = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		swapped = 0;
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				swapped = 1;
+			}
+		}
+		if (swapped == 0)
+			break;
+
+		print_array(array, size);
+	}
+}
---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - that sorts a doubly linked list of int
+ * list: a pointer to the first element of a linkd list
+ *
+*/
+
+void insertion_sort_list(listint_t **list)
+{
+	
+
+
+}
