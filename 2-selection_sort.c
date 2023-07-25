181a8eb1a192195332159ae69e6f535e9184196c
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-07-25 13:42:34 -0700

    solve task no 3

---
+++ 2-selection_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of integers using selection sort
+ * @array: array input
+ * @size: size of array
+*/
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min_idx;
+	int temp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
+			if (array[j] < array[min_idx])
+				min_idx = j;
+		if (min_idx != i)
+		{
+			temp = array[min_idx];
+			array[min_idx] = array[i];
+			array[i] = temp;
+			print_array(array, size);
+		}
+	}
+}
